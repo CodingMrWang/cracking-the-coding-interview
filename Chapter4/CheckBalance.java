186589ec982c58428612495759d13c75d24c56b9
CodingMrWang <zexianw@andrew.cmu.edu>
2018-09-30 02:24:11 -0400

    update c4

--- Chapter4/CheckBalance.java
+++ Chapter4/CheckBalance.java
@@ -1,1 +1,21 @@
-public Check
+public CheckBalance {
+	boolean result = true;
+	public boolean solution(Node root) {
+		if (root == null) {
+			return false;
+		}
+		getHeight(root);
+		return result;
+	}
+	public int getHeight(Node root) {
+		if (root == null) {
+			return 0;
+		}
+		int leftheight = getHeight(root.left);
+		int rightheight = getHeight(root.right);
+		if (Math.abs(leftheight - rightheight) > 1) {
+			result = false;
+		}
+		return Math.max(leftheight, rightheight) + 1;
+	}
+}
